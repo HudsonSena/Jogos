075dca4d967145d0147c097d7c740fa51cb83e50
Hudson Sena <107481318+HudsonSena@users.noreply.github.com>
2025-12-02 15:36:40 -0300

    update jogoRPG002

--- JogoRPG002/render.c
+++ JogoRPG002/render.c
@@ -12,15 +12,25 @@
 
 void desenha_tabuleiro(ALLEGRO_FONT *font) {
     int i; int start_x = 50; int start_y = 100; int cell_size = 80; int spacing = 20; int cells_per_row = 5;
-    ALLEGRO_COLOR cor_jogador = al_map_rgb(255, 0, 0); ALLEGRO_COLOR cor_base = al_map_rgb(150, 150, 150); ALLEGRO_COLOR cor_texto = al_map_rgb(0, 0, 0);           
+    ALLEGRO_COLOR cor_jogador = al_map_rgb(255, 0, 0);
+    ALLEGRO_COLOR cor_base = al_map_rgb(90, 90, 90);
+    ALLEGRO_COLOR cor_texto = al_map_rgb(0, 0, 0);
+    ALLEGRO_COLOR cor_principal = al_map_rgb(200, 200, 255);
+    ALLEGRO_COLOR cor_branco = al_map_rgb(255, 255, 255);
+    ALLEGRO_COLOR cor_dourado = al_map_rgb(255, 205, 55);
+    ALLEGRO_COLOR cor_azul = al_map_rgb(95, 95, 255);
+    ALLEGRO_COLOR cor_vermelho = al_map_rgb(255, 95, 95);
+    ALLEGRO_COLOR cor_vermelho2 = al_map_rgb(150, 50, 50);
+    ALLEGRO_COLOR cor_cinza = al_map_rgb(90, 90, 90);
+    ALLEGRO_COLOR cor_verde = al_map_rgb(55, 205, 55);          
 
     for (i = 0; i < TAMANHO_TABULEIRO; i++) {
         int x = start_x + (i % cells_per_row) * (cell_size + spacing); int y = start_y + (i / cells_per_row) * (cell_size + spacing);
         ALLEGRO_COLOR cor = cor_base;
         if (i == jogador_principal.posicao_atual) cor = cor_jogador; 
-        else if (tabuleiro[i].tipo == CASA_BATALHA) cor = al_map_rgb(200, 50, 50); 
-        else if (tabuleiro[i].tipo == CASA_GEMA) cor = al_map_rgb(0, 150, 255);
-        else if (tabuleiro[i].tipo == CASA_CHEGADA) cor = al_map_rgb(0, 255, 0);
+        else if (tabuleiro[i].tipo == CASA_BATALHA) cor = cor_vermelho2; 
+        else if (tabuleiro[i].tipo == CASA_GEMA) cor = cor_azul;
+        else if (tabuleiro[i].tipo == CASA_CHEGADA) cor = cor_verde;
         
         al_draw_filled_rectangle(x, y, x + cell_size, y + cell_size, cor);
         al_draw_rectangle(x, y, x + cell_size, y + cell_size, al_map_rgb(255, 255, 255), 1);
@@ -44,29 +54,38 @@ void desenha_menu(ALLEGRO_FONT *font) {
 }
 
 void desenha_hud(ALLEGRO_FONT *font, Jogador *j) {
-    int x_start = HUD_X_START; int y_pos = 10; ALLEGRO_COLOR cor_principal = al_map_rgb(200, 200, 255);
+    int x_start = HUD_X_START; int y_pos = 10;
+    ALLEGRO_COLOR cor_principal = al_map_rgb(200, 200, 255);
+    ALLEGRO_COLOR cor_branco = al_map_rgb(255, 255, 255);
+    ALLEGRO_COLOR cor_dourado = al_map_rgb(255, 205, 55);
+    ALLEGRO_COLOR cor_azul = al_map_rgb(95, 95, 255);
+    ALLEGRO_COLOR cor_vermelho = al_map_rgb(255, 95, 95);
+    ALLEGRO_COLOR cor_vermelho2 = al_map_rgb(255, 50, 50);
+    ALLEGRO_COLOR cor_cinza = al_map_rgb(90, 90, 90);
+    ALLEGRO_COLOR cor_verde = al_map_rgb(55, 205, 55);
+      
     int total_ataque = calcular_poder_total(j); 
     
     al_draw_filled_rectangle(x_start - 5, 0, SCREEN_W, SCREEN_H, al_map_rgb(30, 30, 30));
-    al_draw_line(x_start - 5, 0, x_start - 5, SCREEN_H, al_map_rgb(255, 255, 255), 3);
+    al_draw_line(x_start - 5, 0, x_start - 5, SCREEN_H, cor_branco, 3);
 
-    al_draw_text(font, al_map_rgb(255, 255, 0), x_start, y_pos, 0, "--- STATUS ---");
+    al_draw_text(font, cor_branco, x_start, y_pos, 0, "--- STATUS ---");
     y_pos += 40;
 
-    al_draw_textf(font, al_map_rgb(255, 50, 50), x_start, y_pos, 0, "HP: %d", j->vida);
+    al_draw_textf(font, cor_verde, x_start, y_pos, 0, "HP: %d", j->vida);
     y_pos += 30;
     al_draw_textf(font, cor_principal, x_start, y_pos, 0, "Poder Total: %d", total_ataque);
     y_pos += 30;
-    al_draw_textf(font, al_map_rgb(0, 255, 0), x_start, y_pos, 0, "Moedas: %d", j->moedas);
+    al_draw_textf(font, cor_dourado, x_start, y_pos, 0, "Moedas: %d", j->moedas);
     y_pos += 30;
-    al_draw_textf(font, al_map_rgb(0, 255, 0), x_start, y_pos, 0, "Gemas (Itens): %d", j->num_gemas_inventario);
+    al_draw_textf(font, cor_branco, x_start, y_pos, 0, "Gemas (Itens): %d", j->num_gemas_inventario);
     y_pos += 40;
-    al_draw_textf(font, al_map_rgb(255, 255, 255), x_start, y_pos, 0, "SCORE: %d", j->score);
+    al_draw_textf(font, cor_branco, x_start, y_pos, 0, "SCORE: %d", j->score);
     y_pos += 40;
     
     al_draw_text(font, cor_principal, x_start, y_pos, 0, "Arma Equip.:");
     y_pos += 30;
-    al_draw_text(font, al_map_rgb(150, 200, 255), x_start, y_pos, 0, (j->id_arma_equipada > 0) ? todas_as_armas[j->id_arma_equipada - 1].nome : "Nenhuma");
+    al_draw_text(font, cor_branco, x_start, y_pos, 0, (j->id_arma_equipada > 0) ? todas_as_armas[j->id_arma_equipada - 1].nome : "Nenhuma");
     // desenha_objetivos deve ser chamada aqui.
 }
 
